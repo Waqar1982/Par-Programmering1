0e902001e430545a173d3f2bdb815b3b82d60c3c
Waqar1982 <waquar82@gmail.com>
2025-05-14 13:07:00 +0200

    Add project files.

---
+++ Par Programmering1/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_Programmering1
+{
+    internal class person
+
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string FavoriteFood { get; set; }
+        public string Hobby { get; }
+     
+
+        public person(string name, int age, string favoriteFood, string hobby)
+        {
+            Name = name;
+            Age = age;
+            FavoriteFood = favoriteFood;
+            Hobby = hobby;
+           
+
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Hei,jeg heter {Name}., Jeg er  {Age} år gammel., Jeg elsker {FavoriteFood}.,Jeg liker {Hobby}");
+           
+
+        }
+        
+    }
+
+}
---
+++ Par Programmering1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Par_Programmering1;
+
+var waquar = new person(" Waquar Yousaf.",43, "Pakistansk Mat", "å se og spille Fotball.");
+
+
+var zhouLi = new person(" Zhou Li.", 36, "Chinesisk Mat", "å dyrke grønnsake.");
+
+waquar.DisplayInfo();
+zhouLi.DisplayInfo();
